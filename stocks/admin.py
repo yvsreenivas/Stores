d7056181450b46c517c843be85024070f639ac94
yvsreenivas <yvsreenivas@yahoo.co.in>
2021-02-06 17:16:34 +0530

    2nd Commit

---
+++ stocks/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .forms import StockCreateForm
+from .models import Stock
+
+
+class StockCreateAdmin(admin.ModelAdmin):
+   list_display = ['category', 'item_name', 'quantity']
+   form = StockCreateForm
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+
+
+admin.site.register(Stock, StockCreateAdmin)
